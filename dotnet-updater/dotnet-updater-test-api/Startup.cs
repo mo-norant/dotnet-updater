ef209a16a6708921824311c05a88190fedcf203f
mo <mo@diflexmo.be>
2021-07-27 12:39:14 +0200

    dotnet version 6

--- dotnet-updater/dotnet-updater-test-api/Startup.cs
+++ dotnet-updater/dotnet-updater-test-api/Startup.cs
@@ -15,7 +15,7 @@ namespace dotnet_updater_test_api
 {
     public class Startup
     {
-        const string version = "1.0.5";
+        const string version = "1.0.6";
 
         public Startup(IConfiguration configuration)
         {
