22d14aa98fbd5b7ea0fd250f09b3535d5a4b2a53
mo <mo@diflexmo.be>
2021-07-27 13:30:43 +0200

    version 1.0.14

--- dotnet-updater/dotnet-updater-test-api/Startup.cs
+++ dotnet-updater/dotnet-updater-test-api/Startup.cs
@@ -15,7 +15,7 @@ namespace dotnet_updater_test_api
 {
     public class Startup
     {
-        const string version = "1.0.13";
+        const string version = "1.0.14";
 
         public Startup(IConfiguration configuration)
         {
