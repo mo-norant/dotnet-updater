c29043ebc7a27e9d70f5ea8ed790b6317a89306e
mo <mo@diflexmo.be>
2021-07-28 15:11:43 +0200

    version 1.0.20

--- dotnet-updater/dotnet-updater-test-api/Startup.cs
+++ dotnet-updater/dotnet-updater-test-api/Startup.cs
@@ -15,7 +15,7 @@ namespace dotnet_updater_test_api
 {
     public class Startup
     {
-        const string version = "1.0.19";
+        const string version = "1.0.20";
 
         public Startup(IConfiguration configuration)
         {
@@ -51,54 +51,5 @@ namespace dotnet_updater_test_api
                 });
             });
         }
-
-        /*
-         McDonald’s rolled out its famous Szechuan sauce across the US on Monday.
-“Rick and Morty” fans rioted in October after the fast-food chain gave away Szechuan sauce across the US – and demand far exceeded supply.
-The Szechuan sauce is good, but the goopy sweet-and-sour-style sauce is far from riot-worthy.
-McDonald’s Szechuan sauce is finally on the fast-food chain’s menu after months of petitions, bidding wars, and riots in fast-food parking lots.
-
-On Monday, McDonald’s began serving Szechuan sauce at every location across the US.
-
-In October, McDonald’s gave away the sauce at select locations after immense demand from fans of the cartoon show “Rick and Morty.” The limited-edition sauce had appeared on an episode of the Adult Swim show.
-        
-         “Rick and Morty” fans swarmed to those locations, only to find that each restaurant had only 20 to 40 sauce packets and 10 special-edition posters. The diehard fans, some of whom had shown up at the locations as early as 4 a.m., were furious at McDonald’s.
-
-On Monday, McDonald’s wasn’t taking any chances and announced it would be distributing 20 million sauce packs across the country. When we arrived at McDonald’s on Monday, however, there was no sign of the crowds and riots that plagued the fast-food chain when it announced a super-limited return of the sauce in October.
-
-Living up to the degree of hype that Szechuan sauce had experienced is a Herculean task. McDonald’s locations literally had to call the police because “Rick and Morty” fans were rioting when they didn’t get a taste of the sauce. DJ deadmau5 spent roughly $15,000 on a jug of the stuff.
-         
-         “Rick and Morty” fans swarmed to those locations, only to find that each restaurant had only 20 to 40 sauce packets and 10 special-edition posters. The diehard fans, some of whom had shown up at the locations as early as 4 a.m., were furious at McDonald’s.
-
-On Monday, McDonald’s wasn’t taking any chances and announced it would be distributing 20 million sauce packs across the country. When we arrived at McDonald’s on Monday, however, there was no sign of the crowds and riots that plagued the fast-food chain when it announced a super-limited return of the sauce in October.
-
-Living up to the degree of hype that Szechuan sauce had experienced is a Herculean task. McDonald’s locations literally had to call the police because “Rick and Morty” fans were rioting when they didn’t get a taste of the sauce. DJ deadmau5 spent roughly $15,000 on a jug of the stuff.
-         
-         “Rick and Morty” fans swarmed to those locations, only to find that each restaurant had only 20 to 40 sauce packets and 10 special-edition posters. The diehard fans, some of whom had shown up at the locations as early as 4 a.m., were furious at McDonald’s.
-
-On Monday, McDonald’s wasn’t taking any chances and announced it would be distributing 20 million sauce packs across the country. When we arrived at McDonald’s on Monday, however, there was no sign of the crowds and riots that plagued the fast-food chain when it announced a super-limited return of the sauce in October.
-
-Living up to the degree of hype that Szechuan sauce had experienced is a Herculean task. McDonald’s locations literally had to call the police because “Rick and Morty” fans were rioting when they didn’t get a taste of the sauce. DJ deadmau5 spent roughly $15,000 on a jug of the stuff.
-     
-        
-        “Rick and Morty” fans swarmed to those locations, only to find that each restaurant had only 20 to 40 sauce packets and 10 special-edition posters. The diehard fans, some of whom had shown up at the locations as early as 4 a.m., were furious at McDonald’s.
-
-On Monday, McDonald’s wasn’t taking any chances and announced it would be distributing 20 million sauce packs across the country. When we arrived at McDonald’s on Monday, however, there was no sign of the crowds and riots that plagued the fast-food chain when it announced a super-limited return of the sauce in October.
-
-Living up to the degree of hype that Szechuan sauce had experienced is a Herculean task. McDonald’s locations literally had to call the police because “Rick and Morty” fans were rioting when they didn’t get a taste of the sauce. DJ deadmau5 spent roughly $15,000 on a jug of the stuff.
-
-        “Rick and Morty” fans swarmed to those locations, only to find that each restaurant had only 20 to 40 sauce packets and 10 special-edition posters. The diehard fans, some of whom had shown up at the locations as early as 4 a.m., were furious at McDonald’s.
-
-On Monday, McDonald’s wasn’t taking any chances and announced it would be distributing 20 million sauce packs across the country. When we arrived at McDonald’s on Monday, however, there was no sign of the crowds and riots that plagued the fast-food chain when it announced a super-limited return of the sauce in October.
-
-Living up to the degree of hype that Szechuan sauce had experienced is a Herculean task. McDonald’s locations literally had to call the police because “Rick and Morty” fans were rioting when they didn’t get a taste of the sauce. DJ deadmau5 spent roughly $15,000 on a jug of the stuff.
-
-        “Rick and Morty” fans swarmed to those locations, only to find that each restaurant had only 20 to 40 sauce packets and 10 special-edition posters. The diehard fans, some of whom had shown up at the locations as early as 4 a.m., were furious at McDonald’s.
-
-On Monday, McDonald’s wasn’t taking any chances and announced it would be distributing 20 million sauce packs across the country. When we arrived at McDonald’s on Monday, however, there was no sign of the crowds and riots that plagued the fast-food chain when it announced a super-limited return of the sauce in October.
-
-Living up to the degree of hype that Szechuan sauce had experienced is a Herculean task. McDonald’s locations literally had to call the police because “Rick and Morty” fans were rioting when they didn’t get a taste of the sauce. DJ deadmau5 spent roughly $15,000 on a jug of the stuff.
-
-         */
     }
 }
