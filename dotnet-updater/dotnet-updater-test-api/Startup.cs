e4b46f1bb95aa5a0d6d0302aca46fb157c5b4e5f
mo <mo@diflexmo.be>
2021-07-28 15:07:13 +0200

    version altered

--- dotnet-updater/dotnet-updater-test-api/Startup.cs
+++ dotnet-updater/dotnet-updater-test-api/Startup.cs
@@ -15,7 +15,7 @@ namespace dotnet_updater_test_api
 {
     public class Startup
     {
-        const string version = "1.0.18";
+        const string version = "1.0.19";
 
         public Startup(IConfiguration configuration)
         {
@@ -51,5 +51,16 @@ namespace dotnet_updater_test_api
                 });
             });
         }
+
+        /*
+         McDonald’s rolled out its famous Szechuan sauce across the US on Monday.
+“Rick and Morty” fans rioted in October after the fast-food chain gave away Szechuan sauce across the US – and demand far exceeded supply.
+The Szechuan sauce is good, but the goopy sweet-and-sour-style sauce is far from riot-worthy.
+McDonald’s Szechuan sauce is finally on the fast-food chain’s menu after months of petitions, bidding wars, and riots in fast-food parking lots.
+
+On Monday, McDonald’s began serving Szechuan sauce at every location across the US.
+
+In October, McDonald’s gave away the sauce at select locations after immense demand from fans of the cartoon show “Rick and Morty.” The limited-edition sauce had appeared on an episode of the Adult Swim show.
+        */
     }
 }
