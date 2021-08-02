8268fbe604e96696fca94b495a695e3638a9f90b
mo <mo@diflexmo.be>
2021-08-02 17:42:31 +0200

    versie 42

--- dotnet-updater/dotnet-updater-test-api/Startup.cs
+++ dotnet-updater/dotnet-updater-test-api/Startup.cs
@@ -15,7 +15,7 @@ namespace dotnet_updater_test_api
 {
     public class Startup
     {
-        const string version = "1.0.41";
+        const string version = "1.0.42";
 
         public Startup(IConfiguration configuration)
         {
