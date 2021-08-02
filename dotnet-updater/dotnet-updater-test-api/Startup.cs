91ea114e499e6c489bfb5cced5a4c2a96d8a18a6
mo <mo@diflexmo.be>
2021-08-02 18:16:29 +0200

    version 49

--- dotnet-updater/dotnet-updater-test-api/Startup.cs
+++ dotnet-updater/dotnet-updater-test-api/Startup.cs
@@ -15,7 +15,7 @@ namespace dotnet_updater_test_api
 {
     public class Startup
     {
-        const string version = "1.0.48";
+        const string version = "1.0.49";
 
         public Startup(IConfiguration configuration)
         {
