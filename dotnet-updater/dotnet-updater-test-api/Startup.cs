a97bbb3928198eef68e271080d61b5593108298b
mo <mo@diflexmo.be>
2021-08-03 17:19:28 +0200

    version 32

--- dotnet-updater/dotnet-updater-test-api/Startup.cs
+++ dotnet-updater/dotnet-updater-test-api/Startup.cs
@@ -8,7 +8,7 @@ namespace dotnet_updater_test_api
 {
     public class Startup
     {
-        const string version = "1.0.31";
+        const string version = "1.0.32";
 
         public Startup(IConfiguration configuration)
         {
