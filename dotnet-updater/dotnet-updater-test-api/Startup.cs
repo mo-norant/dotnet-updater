9d5ccbcf0c989f64bb68b39ecb602a7774174ffc
mo <mo@diflexmo.be>
2021-08-05 16:43:42 +0200

    version 52

--- dotnet-updater/dotnet-updater-test-api/Startup.cs
+++ dotnet-updater/dotnet-updater-test-api/Startup.cs
@@ -8,7 +8,7 @@ namespace dotnet_updater_test_api
 {
     public class Startup
     {
-        const string version = "1.0.51";
+        const string version = "1.0.52";
 
         public Startup(IConfiguration configuration)
         {
